c2335675f455f8529d987eaef8055917d81d6c4b
basverweij <bas@off-sync.com>
2017-08-10 23:23:01 +0200

    Completed load balancer.

--- loadbalancers/load_balancer.go
+++ loadbalancers/load_balancer.go
@@ -2,8 +2,12 @@ package loadbalancers
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
+
+	"github.com/vulcand/oxy/forward"
+	"github.com/vulcand/oxy/roundrobin"
 )
 
 // LoadBalancer implements the Load Balancer interface.
@@ -13,10 +17,32 @@ type LoadBalancer struct {
 // UpsertService sets the urls for a service. It returns an http.Handler
 // that can be used to make requests to for this service.
 func (l *LoadBalancer) UpsertService(name string, urls ...*url.URL) (http.Handler, error) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain")
-		fmt.Fprintf(w, "service: %s\nurls: %v", name, urls)
-	}), nil
+	fwd, err := forward.New()
+	if err != nil {
+		return nil, err
+	}
+
+	lb, err := roundrobin.New(fwd)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, u := range urls {
+		addrs, err := net.LookupHost(u.Hostname())
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			u2 := &url.URL{}
+			*u2 = *u
+			u2.Host = fmt.Sprintf("%s:%s", addr, u.Port())
+
+			lb.UpsertServer(u2)
+		}
+	}
+
+	return lb, nil
 }
 
 // DeleteService deletes the service from the load balancer.
